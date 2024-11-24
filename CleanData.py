615fc7cd46116c5ef62019e4ff408790aff3ed20
ncdavid2 <ncdavid@web.de>
2024-11-24 19:13:50 +0000

    Charts

--- CleanData.py
+++ CleanData.py
@@ -8,10 +8,13 @@ print("Columns before deletion:")
 print(df.columns)
 
 # Delete a specific column
-columns_to_delete = ['Data_Value_Footnote_Symbol', 'Data_Value_Footnote', 'Low_Confidence_Limit',
-                     'Data_Value_Type', 'High_Confidence_Limit', 'Numerator', 'Sample_Size', 'DataValueTypeID', 'GeoLocation', 'Geographic Level']
+columns_to_delete = ['Data_Value_Footnote_Symbol', 'Data_Value_Footnote', 'Low_Confidence_Limit', 'TopicID', 'CategoryID', 'QuestionID', 'ResponseID',
+                     'AgeID', 'GenderID', 'Data_Value_Type', 'High_Confidence_Limit', 'Numerator', 'DataValueTypeID', 'GeoLocation', 'Geographic Level',
+                     'RaceEthnicityID', 'RiskFactorID', 'RiskFactorResponseID', 'Datasource', 'RiskFactor', 'RiskFactorResponse', 'LocationID']
 
 df = df.drop(columns=columns_to_delete)
+
+df.dropna(subset=['Sample_Size'], inplace=True)
 
 # Print the column names after deletion
 print("\nColumns after deletion:")
@@ -21,3 +24,4 @@ df['Data_Value'] = df['Data_Value'].fillna(0)
 
 # Save the updated DataFrame back to a CSV file (optional)
 df.to_csv('updated_NSCH_Vision_Health_Data.csv', index=False)
+
